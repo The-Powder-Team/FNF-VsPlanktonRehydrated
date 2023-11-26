50722a36756b42e3bd1725a97fe833e7b86bbc74
DetectiveBaldi <86160807+CaptainBaldi@users.noreply.github.com>
2023-11-26 10:29:51 -0500

    Update Paths.hx
    add `posInfos` to `Paths.image`

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -215,7 +215,7 @@ class Paths
 	}
 
 	public static var currentTrackedAssets:Map<String, FlxGraphic> = [];
-	static public function image(key:String, ?library:String = null, ?allowGPU:Bool = true):FlxGraphic
+	static public function image(key:String, ?library:String = null, ?allowGPU:Bool = true, ?posInfos:haxe.PosInfos):FlxGraphic
 	{
 		var bitmap:BitmapData = null;
 		var file:String = null;
@@ -248,7 +248,7 @@ class Paths
 			if(retVal != null) return retVal;
 		}
 
-		trace('oh no its returning null NOOOO ($file)');
+		trace('oh no its returning null NOOOO ($file) (' + posInfos.fileName + ', ' + posInfos.lineNumber + ')');
 		return null;
 	}
 
